46d6515e54b340fc55c7791b2d7fb84ba2b01ff5
Ricardo Pérez <ricpelo@gmail.com>
2023-11-23 19:04:14 +0100

    Versión final

--- ejer6estructurada.py
+++ ejer6estructurada.py
@@ -13,6 +13,21 @@ def pedir_numero() -> int:
         except ValueError:
             print('El dato introducido no es un número.')
 
+
+def validar_numero(n: int, ini: int, fin: int) -> bool:
+    """Comprueba si un número está dentro de un rango."""
+    return n in range(ini, fin + 1)
+
+
+def pedir_numero_valido() -> int:
+    """Solicita un número válido al usuario (entre 0 y 10)."""
+    while True:
+        n = pedir_numero()
+        if validar_numero(n, 0, 10):
+            return n
+        print('El número no está comprendido entre 0 y 10.')
+
+
 def imprimir_tabla_multiplicar(n: int) -> None:
     """Imprime la tabla de multiplicar de n."""
     i = 0
@@ -20,15 +35,6 @@ def imprimir_tabla_multiplicar(n: int) -> None:
         print(n, 'x', i, '=', i * n)
         i += 1
 
-def validar_numero(n: int, ini: int, fin: int) -> bool:
-    """Comprueba si un número está dentro de un rango."""
-    if n not in range(ini, fin + 1):
-        return False
-    return True
 
-while True:
-    numero = pedir_numero()
-    if validar_numero(numero, 0, 10):
-        break
-    print('El número no está comprendido entre 0 y 10.')
+numero = pedir_numero_valido()
 imprimir_tabla_multiplicar(numero)
