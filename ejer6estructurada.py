2ccad70b46d359d41362a850e038e75b90668b28
Ricardo Pérez <ricpelo@gmail.com>
2023-11-23 18:52:45 +0100

    Añade docstrings

--- ejer6estructurada.py
+++ ejer6estructurada.py
@@ -1,8 +1,11 @@
-# 6. Escribir un programa que muestre por pantalla la tabla
-# de multiplicar de un número comprendido entre 0 y 10,
-# introducido por teclado.
+"""
+6. Escribir un programa que muestre por pantalla la tabla
+de multiplicar de un número comprendido entre 0 y 10,
+introducido por teclado.
+"""
 
 def pedir_numero() -> int:
+    """Solicita un número al usuario."""
     while True:
         try:
             n = int(input('Introduzca el número: '))
@@ -11,14 +14,15 @@ def pedir_numero() -> int:
             print('El dato introducido no es un número.')
 
 def imprimir_tabla_multiplicar(n: int) -> None:
+    """Imprime la tabla de multiplicar de n."""
     i = 0
     while i <= 10:
         print(n, 'x', i, '=', i * n)
         i += 1
 
 def validar_numero(n: int, ini: int, fin: int) -> bool:
+    """Comprueba si un número está dentro de un rango."""
     if n not in range(ini, fin + 1):
-        print('El número debe estar comprendido entre 0 y 10.')
         return False
     return True
 
