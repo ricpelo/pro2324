337ae9fa2e91486ac589ed4a063b994841593b36
Ricardo Pérez <ricpelo@gmail.com>
2023-11-23 18:14:53 +0100

    Ejercicio 6 estructurada después

--- ejer6estructurada.py
+++ ejer6estructurada.py
@@ -2,23 +2,29 @@
 # de multiplicar de un número comprendido entre 0 y 10,
 # introducido por teclado.
 
-def pedir_numero_valido():
+def pedir_numero() -> int:
     while True:
         try:
             n = int(input('Introduzca el número: '))
-            if n not in range(0, 11):
-                print('El número debe estar comprendido entre 0 y 10.')
-                continue
             return n
         except ValueError:
-            print('El dato introducido no es un número válido.')
+            print('El dato introducido no es un número.')
 
-
-def imprimir_tabla_multiplicar(n):
+def imprimir_tabla_multiplicar(n: int) -> None:
     i = 0
     while i <= 10:
         print(n, 'x', i, '=', i * n)
         i += 1
 
-numero = pedir_numero_valido()
+def validar_numero(n: int, ini: int, fin: int) -> bool:
+    if n not in range(ini, fin + 1):
+        print('El número debe estar comprendido entre 0 y 10.')
+        return False
+    return True
+
+while True:
+    numero = pedir_numero()
+    if validar_numero(numero, 0, 10):
+        break
+    print('El número no está comprendido entre 0 y 10.')
 imprimir_tabla_multiplicar(numero)
